d1cf744e7d417950cc790577a3061e51f2e64113
Jindy01 <vip.fsafds@mail.ru>
2023-09-04 22:01:55 +0300

    NewReactProject

---
+++ myreact/src/components/screens/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>
+        Cars catalog
+      </h1>
+      <div>
+        <div className={styles.item }>
+            <h2>Cars</h2>
+            <p>Cost : $ 100к </p>
+            <button>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
