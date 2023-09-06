b7aea8065e76eaeb92488926b5f70595182e5859
Jindy01 <vip.fsafds@mail.ru>
2023-09-06 18:55:11 +0300

    TestFile

--- myreact/src/components/screens/home/Home.jsx
+++ myreact/src/components/screens/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
+import car from '/img/Car-1.jpg'
 
 function Home() {
   return (
@@ -9,7 +10,8 @@ function Home() {
       </h1>
       <div>
         <div className={styles.item} >
-          <img src='' alt="" />
+          {/* <img src='D:\OneDrive\Рабочий стол\CourseForReact\myreact\src\img\Car-1.jpg' alt="" /> */}
+          <img src="D:\OneDrive\Рабочий стол\CourseForReact\myreact\src\img\Car-1.jpg" alt="" />
           <h2>Cars</h2>
           <p>Cost : $ 100к </p>
           <button>Read More</button>
