d9e334da8de840d281befe76358cb9dfb340f38d
Jindy01 <vip.fsafds@mail.ru>
2023-09-08 21:42:13 +0300

    New Component

--- myreact/src/components/screens/home/create-car-form/CrateCarForm.jsx
+++ myreact/src/components/screens/home/create-car-form/CrateCarForm.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../create-car-form/CreateCarForm.module.css'
 
 const CreateCarForm = () => {
+
+    const [name, setName] = useState('');
+
+    const cteateCar = () => {
+        console.log(name);
+    }
+
     return (
         <form className={styles.form}>
-            <input type="text" placeholder='Name' />
+            <input type="text"
+                placeholder='Name'
+                onChange={e => setName(e.target.value)}
+                value={name} />
+                <input type="text"
+                placeholder='Name'
+                onChange={e => setName(e.target.value)}
+                value={name} />
+                <input type="text"
+                placeholder='Name'
+                onChange={e => setName(e.target.value)}
+                value={name} />
         </form>
     )
 }
