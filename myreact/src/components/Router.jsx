03354c648f160baf60701ff2443bf67f39ffdf89
Jin dy <Jindy01>
2023-09-11 21:06:04 +0300

    Repair Projetc

--- myreact/src/components/Router.jsx
+++ myreact/src/components/Router.jsx
@@ -4,11 +4,13 @@ import CarDetail from "./screens/home/car-detail/CarDetail";
 
 const Router = () => {
     return <BrowserRouter>
+
         <Routes>
             <Route element={<Home/>} path="/" />
             <Route element={<CarDetail/>} path="/car/:id"/>
             <Route path="*" element={<div> Not Found</div>} />
         </Routes>
+        
     </BrowserRouter>
 }
 
